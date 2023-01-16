31c041e66ed8c82e9d726f67967e0156c81c9c78
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2023-01-16 19:06:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,15 +18,14 @@ class script(object):
 Mʏ Nᴀᴍᴇ Is <a href=https://t.me/Movie_search_hex_bot>Aadhya</a>, I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs, Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ 😍</b>\n
 \nEɴᴛᴇʀ Mᴏᴠɪᴇ Nᴀᴍᴇ ﹠ Wᴀɪᴛ Fᴇᴡ sᴇᴄᴏɴᴅs 😊\n\n
 Bᴏᴛ Dᴇᴠʟᴏᴘᴇᴅ Wɪᴛʜ Lᴏᴠᴇ Bʏ M4U Vɪᴅᴇᴏ\n\n👇ғᴏʟʟᴏᴡ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʙᴇʟᴏᴡ👇"""
-
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """<b><i>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/unique_coders_x><b>unique coders</b></a>\n
-👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://GitHub.com/anjan-budige><b>Altruist</b></a>\n
+    ABOUT_TXT = """<b><i>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Search_zone_bot><b>Search Bot</b></a>\n
+👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/GreyMatter_Owner><b>GreyMatter</b></a>\n
 📝 ʟᴀɴɢᴜᴀɢᴇ : ᴘʏʀᴏɢʀᴀᴍ\n
 📚 ꜰʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏᴛʜᴏɴ 3\n
-📡 ʜᴏsᴛᴇᴅ ᴏɴ : VPS\n
-📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Unique_coders_x><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
+📡 ʜᴏsᴛᴇᴅ ᴏɴ : ʜᴇʀᴏᴋᴜ\n
+📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/GreyMatter_Bots><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
 🌟 ᴠᴇʀsɪᴏɴ : ᴠ 4.0\n</b></i>"""
 
     MANUELFILTER_TXT = """Help: <b>Filters</b>
